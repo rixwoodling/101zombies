da76744bed3a0a15aaf8d696fce3f562768f57a5
Rix <rixwoodling@gmail.com>
2025-04-22 11:59:32 -0700

    Update 101zombies.c

--- 101zombies.c
+++ 101zombies.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>  // for rand()
+#include <time.h>    // for seeding random numbers
 
 void show_help() {
     printf("\n101 ZOMBIES\n");
@@ -10,5 +12,29 @@ void show_help() {
     printf("for medikits when running. How-\n");
     printf("ever, constant running is for\n");
     printf("weak little girls! Good luck.\n\n");
+}
+
+int random_pain() {
+    return (rand() % 15) + 1;  // gives you a number between 1 and 15
+}
+
+int main() {
+    srand(time(NULL));  // seed RNG
+    printf("Zombie hits you for %d damage!\n", random_pain());
+    return 0;
+}
+
+int use_medikit() {
+    int chance = (rand() % 100) + 1;  // 1 to 100
+    if (chance > 30) {
+        int sizes[] = {5, 10};
+        int index = rand() % 2;  // either 0 or 1
+        int size = sizes[index];
+
+        printf("Medikit found! +%d\n", size);
+        return size;
+    } else {
+        return 0;
+    }
 }
 
