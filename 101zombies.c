84dcaa3ec2266e093f3f02c81106a0d127902bf6
Rix <rixwoodling@gmail.com>
2025-04-22 10:54:38 -0700

    Create 101zombies.c help function

---
+++ 101zombies.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void show_help() {
+    printf("\n101 ZOMBIES\n");
+    printf("Choose to FIGHT or RUN away\n");
+    printf("from the zombie horde! Chance\n");
+    printf("for new weapons when fighting.\n");
+    printf("However, consecutive fighting\n");
+    printf("increases zombie damage. Chance\n");
+    printf("for medikits when running. How-\n");
+    printf("ever, constant running is for\n");
+    printf("weak little girls! Good luck.\n\n");
+}
+
